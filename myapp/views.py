7caa8495b305c44246b05bab9a6b5e261825c292
PabloBandeira <correo.pablo.bandeira@gmail.com>
2023-02-27 13:24:47 -0300

    Proyecto Final CoderHouse

---
+++ myapp/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import CreateNewProject, CreateNewTask
+
+# Create your views here.
+
+
+def index(request):
+    title = 'Django Cours!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
+
+
+def hello(request, username):
+    return HttpResponse("<h2>Hello %s</h2>" % username)
+
+
+def about(request):
+    username = 'Pablo'
+    return render(request, 'About.html', {
+        'username': username
+    })
+
+
+def projects(request):
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects/projects.html', {
+        'projects': projects
+    })
+
+
+def tasks(request):
+    # task = get_object_or_404(Task, id=id)
+    tasks = Task.objects.all()
+    return render(request, 'tasks/tasks.html', {
+        'tasks': tasks
+    })
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('tasks')
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {
+            'form': CreateNewProject()
+        })
+    else:
+        Project.objects.create(name=request.POST["name"])
+        return redirect('projects')
