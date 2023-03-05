de3d9f52301e2e98fa387d82f96c72b129616bc8
PabloBandeira <correo.pablo.bandeira@gmail.com>
2023-03-05 19:15:44 -0300

    Cambios al 5/03 V2

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from . import views
 
 
-urlpatterns =[
+urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('hello/<str:username>', views.hello, name="hello"),
-    path('projects/', views.projects, name="projects"), 
-    path('projects/<int:id>', views.project_detail, name="project_detail"),   
+    path('projects/', views.projects, name="projects"),
+    path('projects/<int:id>', views.project_detail, name="project_detail"),
     path('tasks/', views.tasks, name="tasks"),
     path('create_task/', views.create_task, name="create_task"),
-    path('create_project/', views.create_project, name="create_project")
+    path('create_project/', views.create_project, name="create_project"),
+    path('signup/', views.signup, name="signup"),
+    path('logout/', views.logout_view, name="logout"),
+    path('signin/', views.signin, name="signin"),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import CreateNewProject, CreateNewTask
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -60,12 +64,62 @@ def create_project(request):
     else:
         Project.objects.create(name=request.POST["name"])
         return redirect('projects')
-    
-def project_detail(request, id): 
-    project = get_object_or_404(Project, id=id) 
+
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
     tasks = Task.objects.filter(project_id=id)
     print(id)
     return render(request, 'projects/detail.html', {
         'project': project,
         'tasks': tasks
     })
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup/signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # Registramos el usuario
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('index')
+            except IntegrityError:
+                return render(request, 'signup/signup.html', {
+                    'form': UserCreationForm,
+                    "error": 'El usuario ya existe'
+                })
+        return render(request, 'signup/signup.html', {
+            'form': UserCreationForm,
+            "error": 'Las contraseñas no coindicen'
+        })
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'El usuario o la contraseña es incorrecto'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
